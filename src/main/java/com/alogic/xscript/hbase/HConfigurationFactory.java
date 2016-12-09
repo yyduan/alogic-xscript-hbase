c0585244e85cbc8f32f5a2ce75303e1538708670
shangheng <18917372163@189.cn>
2016-12-09 14:27:42 +0800

    提交部分内容

---
+++ src/main/java/com/alogic/xscript/hbase/HConfigurationFactory.java
@@ -0,0 +1,20 @@
+package com.alogic.xscript.hbase;
+
+import com.alogic.pool.impl.Singleton;
+import com.anysoft.util.Properties;
+
+public class HConfigurationFactory extends Singleton {
+
+	@Override
+	public void configure(Properties p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected <pooled> pooled createObject(int priority, int timeout) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
